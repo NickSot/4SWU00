0391d7abc36f4024c12e420b431f33106bca8e02
Zachary Kohnen <me@dusterthefirst.com>
2024-03-10 18:48:29 +0100

    Remove all eddystone packets that are not TLM

--- main/esp_eddystone_api.c
+++ main/esp_eddystone_api.c
@@ -21,119 +21,8 @@
 #include "esp_gap_ble_api.h"
 
 /* Declare static functions */
-static esp_err_t esp_eddystone_uid_received(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res);
-static esp_err_t esp_eddystone_url_received(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res);
-static char *esp_eddystone_resolve_url_scheme(const uint8_t *url_start, const uint8_t *url_end);
 static esp_err_t esp_eddystone_tlm_received(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res);
 static esp_err_t esp_eddystone_get_inform(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res);
-
-/* Eddystone-URL scheme prefixes */
-static const char *eddystone_url_prefix[4] = {
-    "http://www.",
-    "https://www.",
-    "http://",
-    "https://"
-};
-
-/* Eddystone-URL HTTP URL encoding */
-static const char *eddystone_url_encoding[14] = {
-    ".com/",
-    ".org/",
-    ".edu/",
-    ".net/",
-    ".info/",
-    ".biz/",
-    ".gov/",
-    ".com",
-    ".org",
-    ".edu",
-    ".net",
-    ".info",
-    ".biz",
-    ".gov"
-};
-
-/****************** Eddystone-UID **************
-Byte offset	    Field	       Description
-   0	      Frame Type	   Value = 0x00
-   1	     Ranging Data	   Calibrated Tx power at 0 m
-   2	       NID[0]	       10-byte Namespace
-   3	       NID[1]
-   4	       NID[2]
-   5	       NID[3]
-   6	       NID[4]
-   7	       NID[5]
-   8	       NID[6]
-   9	       NID[7]
-   10	       NID[8]
-   11	       NID[9]
-   12	       BID[0]	       6-byte Instance
-   13	       BID[1]
-   14	       BID[2]
-   15	       BID[3]
-   16	       BID[4]
-   17	       BID[5]
-   18	        RFU	           Reserved for future use, must be0x00
-   19	        RFU	           Reserved for future use, must be0x00
-*********************************************/
-/* decode and store received UID */
-static esp_err_t esp_eddystone_uid_received(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res) {
-    uint8_t pos = 0;
-    // 1-byte Ranging Data + 10-byte Namespace + 6-byte Instance
-    if ((len != EDDYSTONE_UID_DATA_LEN) && (len != (EDDYSTONE_UID_RFU_LEN + EDDYSTONE_UID_DATA_LEN))) {
-        // ERROR:uid len wrong
-        return -1;
-    }
-    res->inform.uid.ranging_data = buf[pos++];
-    for (int i = 0; i < EDDYSTONE_UID_NAMESPACE_LEN; i++) {
-        res->inform.uid.namespace_id[i] = buf[pos++];
-    }
-    for (int i = 0; i < EDDYSTONE_UID_INSTANCE_LEN; i++) {
-        res->inform.uid.instance_id[i] = buf[pos++];
-    }
-    return 0;
-}
-
-/* resolve received URL to url_res pointer */
-static char *esp_eddystone_resolve_url_scheme(const uint8_t *url_start, const uint8_t *url_end) {
-    int pos = 0;
-    static char url_buf[100] = { 0 };
-    const uint8_t *p = url_start;
-
-    pos += sprintf(&url_buf[pos], "%s", eddystone_url_prefix[*p++]);
-
-    for (; p <= url_end; p++) {
-        if (esp_eddystone_is_char_invalid((*p))) {
-            pos += sprintf(&url_buf[pos], "%s", eddystone_url_encoding[*p]);
-        } else {
-            pos += sprintf(&url_buf[pos], "%c", *p);
-        }
-    }
-    return url_buf;
-}
-
-/************************** Eddystone-URL *************
-Frame Specification
- Byte offset	 Field	       Description
-    0	       Frame Type	    Value = 0x10
-    1	        TX Power	 Calibrated Tx power at 0 m
-    2	       URL Scheme	   Encoded Scheme Prefix
-    3+	       Encoded URL	    Length 1-17
-*******************************************************/
-/* decode and store received URL, the pointer url_res points to the resolved url */
-static esp_err_t esp_eddystone_url_received(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res) {
-    char *url_res = NULL;
-    uint8_t pos = 0;
-    if (len - EDDYSTONE_URL_TX_POWER_LEN > EDDYSTONE_URL_MAX_LEN) {
-        // ERROR:too long url
-        return -1;
-    }
-    res->inform.url.tx_power = buf[pos++];
-    url_res = esp_eddystone_resolve_url_scheme(buf + pos, buf + len - 1);
-    memcpy(&res->inform.url.url, url_res, strlen(url_res));
-    res->inform.url.url[strlen(url_res)] = '\0';
-    return 0;
-}
 
 /****************** eddystone-tlm ***************
  *  Unencrypted TLM Frame Specification
@@ -177,14 +66,6 @@ static esp_err_t esp_eddystone_tlm_received(const uint8_t *buf, uint8_t len, esp
 static esp_err_t esp_eddystone_get_inform(const uint8_t *buf, uint8_t len, esp_eddystone_result_t *res) {
     static esp_err_t ret = -1;
     switch (res->common.frame_type) {
-        case EDDYSTONE_FRAME_TYPE_UID: {
-            ret = esp_eddystone_uid_received(buf, len, res);
-            break;
-        }
-        case EDDYSTONE_FRAME_TYPE_URL: {
-            ret = esp_eddystone_url_received(buf, len, res);
-            break;
-        }
         case EDDYSTONE_FRAME_TYPE_TLM: {
             ret = esp_eddystone_tlm_received(buf, len, res);
             break;
@@ -224,8 +105,7 @@ esp_err_t esp_eddystone_decode(const uint8_t *buf, uint8_t len, esp_eddystone_re
                 uint16_t type = little_endian_read_16(buf, pos);
                 pos += 2;
                 uint8_t frame_type = buf[pos++];
-                if (type != EDDYSTONE_SERVICE_UUID || !(frame_type == EDDYSTONE_FRAME_TYPE_UID || frame_type == EDDYSTONE_FRAME_TYPE_URL ||
-                                                        frame_type == EDDYSTONE_FRAME_TYPE_TLM)) {
+                if (type != EDDYSTONE_SERVICE_UUID || frame_type != EDDYSTONE_FRAME_TYPE_TLM) {
                     return -1;
                 }
                 res->common.srv_data_type = type;
--- main/esp_eddystone_protocol.h
+++ main/esp_eddystone_protocol.h
@@ -13,16 +13,8 @@
 /* Eddystone definitions */
 #define EDDYSTONE_SERVICE_UUID 0xFEAA
 
-#define EDDYSTONE_FRAME_TYPE_UID 0x00
-#define EDDYSTONE_FRAME_TYPE_URL 0x10
 #define EDDYSTONE_FRAME_TYPE_TLM 0x20
-#define EDDYSTONE_FRAME_TYPE_EID 0x30
-// UID
-#define EDDYSTONE_UID_RANG_DATA_LEN 1
-#define EDDYSTONE_UID_NAMESPACE_LEN 10
-#define EDDYSTONE_UID_INSTANCE_LEN 6
-#define EDDYSTONE_UID_RFU_LEN 2
-#define EDDYSTONE_UID_DATA_LEN (EDDYSTONE_UID_RANG_DATA_LEN + EDDYSTONE_UID_NAMESPACE_LEN + EDDYSTONE_UID_INSTANCE_LEN)
+
 // TLM
 #define EDDYSTONE_TLM_VERSION_LEN 1
 #define EDDYSTONE_TLM_BATTERY_VOLTAGE_LEN 2
@@ -31,26 +23,6 @@
 #define EDDYSTONE_TLM_TIME_LEN 4
 #define EDDYSTONE_TLM_DATA_LEN (EDDYSTONE_TLM_VERSION_LEN + EDDYSTONE_TLM_BATTERY_VOLTAGE_LEN + \
                                 EDDYSTONE_TLM_TEMPERATURE_LEN + EDDYSTONE_TLM_ADV_COUNT_LEN + EDDYSTONE_TLM_TIME_LEN)
-// URL
-#define EDDYSTONE_URL_SCHEME_LEN 1
-#define EDDYSTONE_URL_ENCODED_MAX_LEN 17
-#define EDDYSTONE_URL_MAX_LEN (EDDYSTONE_URL_SCHEME_LEN + EDDYSTONE_URL_ENCODED_MAX_LEN)
-#define EDDYSTONE_URL_TX_POWER_LEN 1
-
-/* Eddystone UID frame */
-typedef struct {
-    int8_t ranging_data; /*<! calibrated Tx power at 0m */
-    uint8_t namespace_id[10];
-    uint8_t instance_id[6];
-    uint8_t reserved[2];
-} __attribute__((packed)) esp_eddystone_uid_t;
-
-/* Eddystone URL frame */
-typedef struct {
-    int8_t tx_power;        /*<! calibrated Tx power at 0m */
-    uint8_t url_scheme;     /*<! encoded scheme prefix */
-    uint8_t encoded_url[0]; /*<! length 1-17 */
-} __attribute__((packed)) esp_eddystone_url_t;
 
 /* Eddystone TLM frame */
 typedef struct {
@@ -81,11 +53,7 @@ typedef struct {
     uint8_t type;  /*<! service data type,must be 0x16 */
     uint16_t uuid; /*<! 16-bit eddystone uuid */
     uint8_t frame_type;
-    union {
-        esp_eddystone_uid_t uid;
-        esp_eddystone_url_t url;
-        esp_eddystone_tlm_t tlm;
-    } u[0];
+    esp_eddystone_tlm_t tlm;
 } __attribute__((packed)) esp_eddystone_frame_t;
 
 /* eddystone packet type */
@@ -94,15 +62,5 @@ typedef struct {
     esp_eddystone_uuid_t uuid;
     esp_eddystone_frame_t frame;
 } __attribute__((packed)) esp_eddystone_packet_t;
-
-/*
- * URLs are written only with the graphic printable characters of the US-ASCII coded character set.
- * The octets 00-20 and 7F-FF hexadecimal are not used.
- * See “Excluded US-ASCII Characters” in RFC 2936.
- *
- */
-static inline bool esp_eddystone_is_char_invalid(int ch) {
-    return (ch >= 0x00 && ch <= 0x20) || (ch >= 0x7f && ch <= 0xff);
-}
 
 #endif /* __ESP_EDDYSTONE_PROTOCOL_H__ */
